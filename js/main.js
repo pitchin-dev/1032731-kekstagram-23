4bed455605f7ee1cb813a29bb8ccdc30a4465c44
Pavel Itchin <itchin.pavel@yandex.ru>
2021-05-29 14:44:45 +0300

    Добавил необходимые условия

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+  if(min >= 0 && max >= 0) {
+    if (max < min) {
+      console.log('Cannot return random number. Check your max number, it must be bigger than min number');
+      return false;
+    } else if (max === min) {
+      console.log('Min and max values are identical');
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    } else {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  } else {
+    console.log('Min and max number must be positive');
+    return false;
+  }
 }
 
 getRandomInt(1, 10);
