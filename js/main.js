64e28461da39f3bf84ac60b5bd7685681c9c73be
Pavel Itchin <itchin.pavel@yandex.ru>
2021-05-29 16:56:01 +0300

    Удалил лишнее

--- js/main.js
+++ js/main.js
@@ -3,30 +3,21 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if(min >= 0 && max >= 0) {
-    if (max < min) {
-      console.log('Cannot return random number. Check your max number, it must be bigger than min number');
-      return false;
-    } else if (max === min) {
-      console.log('Min and max values are identical');
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+  if (min >= 0 && max >= 0 ) {
+    if (max < min || max === min) {
+      return 'Cannot return random number. Check your max number, it must be bigger than min number or Min and max values are identical';
     } else {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
   } else {
-    console.log('Min and max number must be positive');
-    return false;
+    return 'Min and max number must be positive';
   }
 }
 
 getRandomInt(1, 10);
 
 function checkStringLength(string, maxLength) {
-  if (string.length <= maxLength) {
-    return true;
-  } else {
-    return false;
-  }
+  return string.length <= maxLength;
 }
 
 checkStringLength('test', 10);
