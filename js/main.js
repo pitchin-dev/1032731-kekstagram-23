cb983ac9fd6e67a6fbae4fcb1f08ccedc9391f96
Pavel Itchin <itchin.pavel@yandex.ru>
2021-05-30 21:36:41 +0300

    Оптимизировал структуру функции

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (min >= 0 && max >= 0 ) {
-    if (max < min || max === min) {
-      return 'Cannot return random number. Check your max number, it must be bigger than min number or Min and max values are identical';
-    } else {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-  } else {
-    return 'Min and max number must be positive';
+  if (min < 0 || max < 0) {
+    return 'Cannot return random numbers. Min and max number must be positive';
   }
+
+  if (max < min || max === min) {
+    return 'Cannot return random number. Check your max number, it cannot be less or identical to min number';
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 getRandomInt(1, 10);
