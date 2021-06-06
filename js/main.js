3a157b467fe22328a58d136228ebaf3e15a13661
Pavel Itchin <itchin.pavel@yandex.ru>
2021-06-06 14:28:21 +0300

    Написал функции для четвёртого модуля

--- js/main.js
+++ js/main.js
@@ -21,3 +21,53 @@ function checkStringLength(string, maxLength) {
 }
 
 checkStringLength('test', 10);
+
+function createObjectsArray(quantity) {
+  const objectsArray = [];
+  let commentsCounter = 0;
+  const names = [
+    'Павел',
+    'Александр',
+    'Алексей',
+    'Николай',
+    'Пётр',
+    'Виталий',
+    'Анна',
+    'Анастасия',
+    'Ирина',
+    'Ольга',
+    'Ксения',
+    'Алина',
+  ];
+  const comments = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  ];
+
+  function createCommentSection() {
+    return {
+      id: commentsCounter += 1,
+      avatar: `img/avatar-${getRandomInt(1, 6)}`,
+      message: comments[getRandomInt(0, 5)],
+      name: names[getRandomInt(0, 11)],
+    };
+  }
+
+  for (let index = 0; index < quantity; index++) {
+    const obj = {
+      id: index + 1,
+      url: `photos/${index + 1}.jpg`,
+      description: `Тестовое описание фотографии номер ${index + 1}`,
+      likes: getRandomInt(15, 200),
+      comments: new Array(getRandomInt(1, 5)).fill(null).map(() => createCommentSection()),
+    };
+    objectsArray.push(obj);
+  }
+  return objectsArray;
+}
+
+createObjectsArray(25);
