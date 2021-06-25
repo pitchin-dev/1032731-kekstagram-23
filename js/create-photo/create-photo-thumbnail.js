feb700cab65c2a9e1975ce36d7bf66226f248156
Pavel Itchin <itchin.pavel@yandex.ru>
2021-06-25 20:10:02 +0300

    Исправление замечаний

--- js/create-photo/create-photo-thumbnail.js
+++ js/create-photo/create-photo-thumbnail.js
@@ -1,17 +1,22 @@
-import {POSTS} from '../data.js';
+import {posts} from '../data.js';
 
 const picture = document.querySelector('#picture').content.querySelector('.picture');
 const pictures = document.querySelector('.pictures');
-const picturesDescriptionArray = POSTS;
+const picturesDescriptionArray = posts;
 const picturesFragment = document.createDocumentFragment();
 
-picturesDescriptionArray.forEach(({url, likes, comments}) => {
+function createPhotoThumbnail(arr) {
   const pictureElement = picture.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.querySelector('.picture__img').src = arr.url;
+  pictureElement.querySelector('.picture__likes').textContent = arr.likes;
+  pictureElement.querySelector('.picture__comments').textContent = arr.comments.length;
 
   picturesFragment.appendChild(pictureElement);
+}
+
+picturesDescriptionArray.forEach((item) => {
+  createPhotoThumbnail(item);
 });
 
 pictures.appendChild(picturesFragment);
+console.log(pictures);
