2494d67f01a5cb4f1299e6e2387d8e96cf43832f
Pavel Itchin <itchin.pavel@yandex.ru>
2021-06-25 20:29:14 +0300

    Update create-photo-thumbnail.js

--- js/create-photo/create-photo-thumbnail.js
+++ js/create-photo/create-photo-thumbnail.js
@@ -3,19 +3,19 @@ import {posts} from '../data.js';
 const picture = document.querySelector('#picture').content.querySelector('.picture');
 const pictures = document.querySelector('.pictures');
 const picturesDescriptionArray = posts;
-const picturesFragment = document.createDocumentFragment();
 
 function createPhotoThumbnail(arr) {
+  const pictureFragment = document.createDocumentFragment();
   const pictureElement = picture.cloneNode(true);
+
   pictureElement.querySelector('.picture__img').src = arr.url;
   pictureElement.querySelector('.picture__likes').textContent = arr.likes;
   pictureElement.querySelector('.picture__comments').textContent = arr.comments.length;
+  pictureFragment.appendChild(pictureElement);
 
-  picturesFragment.appendChild(pictureElement);
+  return pictureFragment;
 }
 
 picturesDescriptionArray.forEach((item) => {
-  createPhotoThumbnail(item);
+  pictures.appendChild(createPhotoThumbnail(item));
 });
-
-pictures.appendChild(picturesFragment);
