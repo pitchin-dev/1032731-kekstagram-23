c1bf12eecc29cd5dda0e20b9598b16b78caf6099
Pavel Itchin <itchin.pavel@yandex.ru>
2021-07-17 17:37:45 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-import {renderPostsList} from './render-pictures.js';
+import {renderPhotoFilter, renderPostsList} from './render-pictures.js';
 import {getData} from './api.js';
+import {showAlert} from './utils/utils.js';
 import './picture-upload-editor.js';
 
 getData((posts) => {
   renderPostsList(posts);
-});
+  renderPhotoFilter(posts);
+},
+() => showAlert('Не удалось загрузить данные!'));
