d473336f32af53545888cdeed2608fc38b28e0da
Pavel Itchin <itchin.pavel@yandex.ru>
2021-07-17 17:45:47 +0300

    Update render-pictures.js

--- js/render-pictures.js
+++ js/render-pictures.js
@@ -1,13 +1,23 @@
 import {showFullPhoto} from './create-photo-full.js';
+import {debounce} from './utils/debounce.js';
+import {shuffle} from './utils/utils.js';
 
+const RERENDER_DELAY = 500;
+const NUMBER_RANDOM_PICTURES = 10;
 const picturesContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const postListFragment = document.createDocumentFragment();
+const imgFilters = document.querySelector('.img-filters');
+const imgFiltersBtn = document.querySelectorAll('.img-filters__button');
+const defaultPicturesBtn = imgFilters.querySelector('#filter-default');
+const randomPicturesBtn = imgFilters.querySelector('#filter-random');
+const discussedPicturesBtn = imgFilters.querySelector('#filter-discussed');
 let postListCopy = [];
 
 function renderPostsList (posts) {
   postListCopy = posts;
-  posts.forEach(({ url, likes, comments }) => {
+
+  postListCopy.forEach(({ url, likes, comments }) => {
     const postElement = pictureTemplate.cloneNode(true);
     postElement.querySelector('.picture__img').setAttribute('src', url);
     postElement.querySelector('.picture__likes').textContent = likes;
@@ -16,15 +26,55 @@ function renderPostsList (posts) {
   });
 
   picturesContainer.appendChild(postListFragment);
+  imgFilters.classList.remove('img-filters--inactive');
 }
+
+const setFilterDebounced = (debounce(
+  (photoList) => {
+    picturesContainer.querySelectorAll('.picture').forEach((picture) => {
+      picture.remove();
+    });
+    renderPostsList(photoList);
+  },
+  RERENDER_DELAY,
+));
 
 picturesContainer.addEventListener('click', (e) => {
   const target = e.target;
-  if (target.className === 'picture__img') {
+  if (target.closest('.picture__img')) {
     e.preventDefault();
     const post = postListCopy.find(({url}) => url === target.getAttribute('src'));
     showFullPhoto(post);
   }
 });
 
-export {renderPostsList};
+function setFiltersActive (activeButton) {
+  imgFiltersBtn.forEach((imgFilterButton) => {
+    imgFilterButton.classList.remove('img-filters__button--active');
+  });
+  activeButton.classList.add('img-filters__button--active');
+}
+
+function renderPhotoFilter (userPhotos) {
+  defaultPicturesBtn.addEventListener('click', () => {
+    setFiltersActive(defaultPicturesBtn);
+    const defaultPictures = userPhotos.sort((a, b) => a.id > b.id ? 1 : -1);
+    setFilterDebounced(defaultPictures);
+  });
+
+  randomPicturesBtn.addEventListener('click', () => {
+    setFiltersActive(randomPicturesBtn);
+    shuffle(userPhotos);
+    const randomPictures = userPhotos.slice(0, NUMBER_RANDOM_PICTURES);
+    setFilterDebounced(randomPictures);
+  });
+
+  discussedPicturesBtn.addEventListener('click', () => {
+    setFiltersActive(discussedPicturesBtn);
+    const discussedPictures = userPhotos.sort((a, b) => a.comments.length > b.comments.length ? 1 : -1);
+    discussedPictures.reverse();
+    setFilterDebounced(discussedPictures);
+  });
+}
+
+export {renderPostsList, renderPhotoFilter};
