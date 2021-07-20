551df77757c0318e1c7c5aa524d5b17eaa46d590
Pavel Itchin <itchin.pavel@yandex.ru>
2021-07-20 11:21:04 +0300

    Update upload.js

--- js/upload.js
+++ js/upload.js
@@ -1,4 +1,4 @@
-const FILES_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+const TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 const uploadPictureInput = document.querySelector('#upload-file');
 const editPictureModal = document.querySelector('.img-upload__overlay');
@@ -7,7 +7,7 @@ const picturePreview = editPictureModal.querySelector('.img-upload__preview img'
 uploadPictureInput.addEventListener('change', () => {
   const file = uploadPictureInput.files[0];
   const fileName = file.name.toLowerCase();
-  const matches = FILES_TYPES.some((it) => fileName.endsWith(it));
+  const matches = TYPES.some((it) => fileName.endsWith(it));
 
   if (matches) {
     const reader = new FileReader();
